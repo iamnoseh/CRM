0389c692c8febc9342a8becf0d46f6933e8f19d8
Noseh Taghaymurodzoda <nosehtagaymurodzoda@gmail.com>
2025-09-19 21:06:08 +0500

    added new migrations

---
+++ Infrastructure/Migrations/20250919160506_sync_model_Users_and_identity.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class sync_model_Users_and_identity : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<decimal>(
+                name: "DiscountAmount",
+                table: "Payments",
+                type: "numeric(18,2)",
+                precision: 18,
+                scale: 2,
+                nullable: false,
+                defaultValue: 0m);
+
+            migrationBuilder.AddColumn<decimal>(
+                name: "OriginalAmount",
+                table: "Payments",
+                type: "numeric(18,2)",
+                precision: 18,
+                scale: 2,
+                nullable: false,
+                defaultValue: 0m);
+
+            migrationBuilder.CreateTable(
+                name: "StudentGroupDiscounts",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    StudentId = table.Column<int>(type: "integer", nullable: false),
+                    GroupId = table.Column<int>(type: "integer", nullable: false),
+                    DiscountAmount = table.Column<decimal>(type: "numeric(18,2)", precision: 18, scale: 2, nullable: false),
+                    CreatedAt = table.Column<DateTimeOffset>(type: "timestamp with time zone", nullable: false),
+                    UpdatedAt = table.Column<DateTimeOffset>(type: "timestamp with time zone", nullable: false),
+                    IsDeleted = table.Column<bool>(type: "boolean", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_StudentGroupDiscounts", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_StudentGroupDiscounts_Groups_GroupId",
+                        column: x => x.GroupId,
+                        principalTable: "Groups",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_StudentGroupDiscounts_Students_StudentId",
+                        column: x => x.StudentId,
+                        principalTable: "Students",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_StudentGroupDiscount_Student_Group_IsDeleted",
+                table: "StudentGroupDiscounts",
+                columns: new[] { "StudentId", "GroupId", "IsDeleted" });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_StudentGroupDiscounts_GroupId",
+                table: "StudentGroupDiscounts",
+                column: "GroupId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_StudentGroupDiscounts_StudentId",
+                table: "StudentGroupDiscounts",
+                column: "StudentId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "StudentGroupDiscounts");
+
+            migrationBuilder.DropColumn(
+                name: "DiscountAmount",
+                table: "Payments");
+
+            migrationBuilder.DropColumn(
+                name: "OriginalAmount",
+                table: "Payments");
+        }
+    }
+}
