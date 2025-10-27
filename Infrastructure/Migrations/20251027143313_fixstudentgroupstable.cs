0a72e042498e35b2e5e1dadb86404926961178d7
Noseh Taghaymurodzoda <nosehtagaymurodzoda@gmail.com>
2025-10-27 19:33:45 +0500

    Add IsLeft, LeftDate, and LeftReason properties to DataContextModelSnapshot for enhanced student group membership management.

---
+++ Infrastructure/Migrations/20251027143313_fixstudentgroupstable.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class fixstudentgroupstable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "IsLeft",
+                table: "StudentGroups",
+                type: "boolean",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "LeftDate",
+                table: "StudentGroups",
+                type: "timestamp with time zone",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "LeftReason",
+                table: "StudentGroups",
+                type: "text",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "IsLeft",
+                table: "StudentGroups");
+
+            migrationBuilder.DropColumn(
+                name: "LeftDate",
+                table: "StudentGroups");
+
+            migrationBuilder.DropColumn(
+                name: "LeftReason",
+                table: "StudentGroups");
+        }
+    }
+}
